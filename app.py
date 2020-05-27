88ecdc4d16acd31bf064ccfbdde89d576997d87e
Your Name <you@example.com>
2020-05-27 10:21:55 +0430

    started using flask

---
+++ app.py
@@ -0,0 +1,10 @@
+# sms_verification
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main_page():
+    '''
+        This is the main page of the site
+    '''
+    return "Hello"
